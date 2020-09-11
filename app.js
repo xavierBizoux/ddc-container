4c1d3e0657e74a92ed2f02c1d18097d64175777e
Xavier BIZOUX <xavier.bizouxsas.com>
2020-09-11 09:38:02 +0200

    Update application path

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use('/javascripts', express.static(__dirname + '/node_modules/jquery/dist'))
 app.use('/javascripts', express.static(__dirname + '/node_modules/popper.js/dist')); // redirect Popper JS
 app.use('/stylesheets', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
 
-app.use('/ddc', indexRouter);
+app.use('/ddc/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
